dd5d40e33e24969d7e6f377945be8b0f54fb2d9f
SAK <SAK@gmail.com>
2025-05-10 23:50:25 -0400

    Bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/shaikabdulkhadar1.github.io/",
+  base: "/",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
